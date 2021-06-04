b13a645354bdadb6056e460c7d763a163760b937
NMR <77173929+NMR-Code@users.noreply.github.com>
2021-06-04 11:12:25 -0400

    added function for new departments

--- app.js
+++ app.js
@@ -22,46 +22,69 @@ connection.connect(function(err) {
 
 //Function to determine what the user wants to do and trigger the appropiate flow.
 const intro = function() {
+        inquirer.prompt({
+            type: "list",
+            name: "optionList",
+            message: "Select an option below!",
+            choices: ["Create a Department", "Create a Role", "Add an Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View all Employees", "View all Employees by Department", "View all Employees By Manager", "View the total utilized budget of a Department", "Exit"]
+        }).then(function(answers) {
+            switch (answers.intro) {
+                case "Create a Department":
+                    newDepartment();
+                    break;
+                case "Create a Role":
+                    newRole();
+                    break;
+                case "Add an Employee":
+                    newEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;
+                case "View all Employees":
+                    viewAllEmployees();
+                    break;
+                case "View all Employees by Department":
+                    viewAllEmployeesByDepartment();
+                    break;
+                case "View all Employees By Manager":
+                    viewAllEmployeesByManager();
+                    break;
+                case "Exit":
+                    exitApp();
+            }
+        });
+    }
+    //Function to Create a new department
+const newDepartment = function() {
     inquirer.prompt({
-        type: "list",
-        name: "optionList",
-        message: "Select an option below!",
-        choices: ["Create a Department", "Create a Role", "Add an Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View all Employees", "View all Employees by Department", "View all Employees By Manager", "View the total utilized budget of a Department", "Exit"]
+        type: "input",
+        name: "department",
+        message: "Name of new department?"
     }).then(function(answers) {
-        switch (answers.intro) {
-            case "Create a Department":
-                newDepartment();
-                break;
-            case "Create a Role":
-                newRole();
-                break;
-            case "Add an Employee":
-                newEmployee();
-                break;
-            case "Remove Employee":
-                removeEmployee();
-                break;
-            case "Update Employee Role":
-                updateEmployeeRole();
-                break;
-            case "Update Employee Manager":
-                updateEmployeeManager();
-                break;
-            case "View all Employees":
-                viewAllEmployees();
-                break;
-            case "View all Employees by Department":
-                viewAllEmployeesByDepartment();
-                break;
-            case "View all Employees By Manager":
-                viewAllEmployeesByManager();
-                break;
-            case "Exit":
-                exitApp();
+        if (answers.departmentName === "") {
+            console.log("Must enter a name, try again :(");
+            newDepartment();
+        } else {
+            connection.query("INSERT INTO department SET ?", {
+                name: answers.departmentName
+            }, function(err, res) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`Department: "${answers.departmentName}" has been created!`);
+                    intro();
+                }
+            });
         }
     });
-}
-
+};
 
 //Initialize App
 intro();
